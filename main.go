942bfe9ce5a1d2e4b8fbae93db0f356963ac5f4e
s10128109 <user.email>
2019-07-09 17:06:24 +0800

    generate int V1

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 )
 
 var err error
+var nums [1024]string
+var indexNums int
 
 func check(e error) {
 	if e != nil {
@@ -31,6 +33,10 @@ func readThisLine(theLine *bufio.Scanner) [5]string {
 	for i := 0; i < 5; i++ {
 		w[i] = ""
 	}
+	for i := 0; i < len(nums); i++ {
+		nums[i] = ""
+	}
+	indexNums = 0
 	theLine.Split(bufio.ScanWords)
 	for i := 0; theLine.Scan(); i++ {
 		if i < 5 { //先讀標頭格式w[0]~w[5]
@@ -38,13 +44,16 @@ func readThisLine(theLine *bufio.Scanner) [5]string {
 			//fmt.Println(w[i])
 			if i == 0 {
 				if w[0] == "@as" {
-					fmt.Println("人工測資")
+					//fmt.Println("人工測資")
 					i = 4 //標頭結束
 				}
 			}
 		} else {
+			nums[indexNums] = theLine.Text()
+			indexNums++
 			//標頭讀完，把剩下nums[] 讀完
 			//fmt.Print(theLine.Text() + ", ")
+
 		}
 
 	}
@@ -59,21 +68,61 @@ func showData(w [5]string) {
 	//w[2] 範圍(range) 1~99
 	rangeBegin := 0
 	rangeEnd := 0
+	rangeSpecial := 0
+	rangeFormat := ""
+
 	if strings.ContainsAny(w[2], "~") {
 		rangeBegin, err = strconv.Atoi(strings.Split(w[2], "~")[0])
 		if err != nil {
 			panic(err.Error())
 		}
-		rangeEnd, err = strconv.Atoi(strings.Split(w[2], "~")[1])
-		if err != nil {
-			panic(err.Error())
+
+		if strings.ContainsAny(w[2], "&") {
+			rangeFormat = "&"
+			word2 := strings.Split(w[2], "~")[1]
+			rangeEnd, err = strconv.Atoi(strings.Split(word2, rangeFormat)[0])
+			if err != nil {
+				panic(err.Error())
+			}
+			rangeSpecial, err = strconv.Atoi(strings.Split(word2, rangeFormat)[1])
+			if err != nil {
+				panic(err.Error())
+			}
+		} else if strings.ContainsAny(w[2], "*") {
+			rangeFormat = "*"
+			word2 := strings.Split(w[2], "~")[1]
+			rangeEnd, err = strconv.Atoi(strings.Split(word2, rangeFormat)[0])
+			if err != nil {
+				panic(err.Error())
+			}
+			rangeSpecial, err = strconv.Atoi(strings.Split(word2, rangeFormat)[1])
+			if err != nil {
+				panic(err.Error())
+			}
+		} else if strings.ContainsAny(w[2], "^") {
+			rangeFormat = "^"
+			word2 := strings.Split(w[2], "~")[1]
+			rangeEnd, err = strconv.Atoi(strings.Split(word2, rangeFormat)[0])
+			if err != nil {
+				panic(err.Error())
+			}
+			rangeSpecial, err = strconv.Atoi(strings.Split(word2, rangeFormat)[1])
+			if err != nil {
+				panic(err.Error())
+			}
+		} else {
+
+			rangeEnd, err = strconv.Atoi(strings.Split(w[2], "~")[1])
+			if err != nil {
+				panic(err.Error())
+			}
 		}
 	}
 	//w[3] 個數(num)
 	var num int
 	if w[3] == "" {
 		num = 1
-		fmt.Printf("從%d~%d中選%d個數字\n", rangeBegin, rangeEnd, num)
+		//fmt.Printf("從%d~%d中選%d個數字\n", rangeBegin, rangeEnd, num)
 	} else if strings.ContainsAny(w[3], "~") {
 		randBegin := 0
 		randEnd := 0
@@ -87,61 +136,228 @@ func showData(w [5]string) {
 				panic(err.Error())
 			}
 		}
-		rand.Seed(time.Now().UnixNano())
+
 		randNum := rand.Intn(randEnd - randBegin + 1)
 		//fmt.Printf("randNum = %d\n", randNum)
 		num = randNum + randBegin
 
-		fmt.Printf("從%d~%d中選%d~%d個數字, num = %d\n", rangeBegin, rangeEnd, randBegin, randEnd, num)
+		//fmt.Printf("從%d~%d中選%d~%d個數字, num = %d\n", rangeBegin, rangeEnd, randBegin, randEnd, num)
 	} else {
 		num, err = strconv.Atoi(w[3])
 		if err != nil {
 			panic(err.Error())
 		}
-		fmt.Printf("從%d~%d中選%d個數字\n", rangeBegin, rangeEnd, num)
+		//fmt.Printf("從%d~%d中選%d個數字\n", rangeBegin, rangeEnd, num)
 	}
 	//w[4] 顯示格式(format)補零、逗號...
 
-	fmt.Println("最終測資為：")
-	switch w[4] {
-	case "":
-		for i := 0; i < num; i++ {
-			fmt.Printf("%d ", rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)
-		}
-		break
-	case "h":
-		fmt.Printf("%d", num)
-		for i := 0; i < num; i++ {
-			fmt.Printf(" %d", rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)
-		}
-		break
-	case "z":
-		for i := 0; i < num; i++ {
-			fmt.Printf("%d ", rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)
-		}
-		fmt.Print("0")
-		break
-	case "e":
-		for i := 0; i < num; i++ {
-			fmt.Printf("%d", rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)
-		}
-		break
-	case "c":
-		for i := 0; i < num; i++ {
-			if i != 0 {
-				fmt.Print(",")
+	//fmt.Println("最終測資為：")
+	if w[2] == "0" {
+		if w[4] == "g" { //從nums中挑選num個數字，可重複
+			for i := 0; i < num; i++ {
+				idx := rand.Intn(indexNums)
+				fmt.Printf("%s ", nums[idx])
 			}
-			fmt.Printf("%d", rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)
+		} else { //從nums中挑選num個數字，不可重複
+			for i := 0; i < num; i++ {
+				idx := rand.Intn(indexNums)
+				for nums[idx] == "used" {
+					idx++
+					idx = idx % indexNums
+				}
+				fmt.Printf("%s ", nums[idx])
+				nums[idx] = "used"
+			}
 		}
-		break
-	}
+	} else {
+		switch w[4] {
+		case "":
+			switch rangeFormat {
+			case "":
+				for i := 0; i < num; i++ {
+					fmt.Printf("%d ", rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)
+				}
+				break
+			case "&": //間隔rangeSpeical
+				indexNums = 0
+				for i := rangeBegin; i <= rangeEnd; i += rangeSpecial {
+					nums[indexNums] = strconv.Itoa(i)
+					indexNums++
+				}
+				for i := 0; i < num; i++ {
+					idx := rand.Intn(indexNums)
+					fmt.Printf("%s ", nums[idx])
+				}
+				break
+			case "*":
+				for i := 0; i < num; i++ {
+					fmt.Printf("%d ", (rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)*rangeSpecial)
+				}
+				break
+			case "^":
+				for i := 0; i < num; i++ {
+					outData := rand.Intn(rangeEnd-rangeBegin+1) + rangeBegin
+					//fmt.Printf("begin=%d, end=%d, out=%d\n", rangeBegin, rangeEnd, outData)
+					fmt.Printf("%d ", outData*outData)
+				}
+				break
+			}
+			break
 
+		case "h": //前面補上個數
+			fmt.Printf("%d ", num)
+			switch rangeFormat {
+			case "":
+				for i := 0; i < num; i++ {
+					fmt.Printf("%d ", rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)
+				}
+				break
+			case "&":
+				//fmt.Println("間隔晚點想")
+				indexNums = 0
+				for i := rangeBegin; i <= rangeEnd; i += rangeSpecial {
+					nums[indexNums] = strconv.Itoa(i)
+					indexNums++
+				}
+				for i := 0; i < num; i++ {
+					idx := rand.Intn(indexNums)
+					fmt.Printf("%s ", nums[idx])
+				}
+				break
+			case "*":
+				for i := 0; i < num; i++ {
+					fmt.Printf("%d ", (rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)*rangeSpecial)
+				}
+				break
+			case "^":
+				for i := 0; i < num; i++ {
+					outData := rand.Intn(rangeEnd-rangeBegin+1) + rangeBegin
+					//fmt.Printf("begin=%d, end=%d, out=%d\n", rangeBegin, rangeEnd, outData)
+					fmt.Printf("%d ", outData*outData)
+				}
+				break
+			}
+			break
+		case "z": //尾巴補零
+			switch rangeFormat {
+			case "":
+				for i := 0; i < num; i++ {
+					fmt.Printf("%d ", rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)
+				}
+				break
+			case "&":
+				//fmt.Println("間隔晚點想")
+				indexNums = 0
+				for i := rangeBegin; i <= rangeEnd; i += rangeSpecial {
+					nums[indexNums] = strconv.Itoa(i)
+					indexNums++
+				}
+				for i := 0; i < num; i++ {
+					idx := rand.Intn(indexNums)
+					fmt.Printf("%s ", nums[idx])
+				}
+				break
+			case "*":
+				for i := 0; i < num; i++ {
+					fmt.Printf("%d ", (rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)*rangeSpecial)
+				}
+				break
+			case "^":
+				for i := 0; i < num; i++ {
+					outData := rand.Intn(rangeEnd-rangeBegin+1) + rangeBegin
+					//fmt.Printf("begin=%d, end=%d, out=%d\n", rangeBegin, rangeEnd, outData)
+					fmt.Printf("%d ", outData*outData)
+				}
+				break
+			}
+			fmt.Print("0")
+			break
+		case "e": //不留白
+			switch rangeFormat {
+			case "":
+				for i := 0; i < num; i++ {
+					fmt.Printf("%d", rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)
+				}
+				break
+			case "&":
+				//fmt.Println("間隔晚點想")
+				indexNums = 0
+				for i := rangeBegin; i <= rangeEnd; i += rangeSpecial {
+					nums[indexNums] = strconv.Itoa(i)
+					indexNums++
+				}
+				for i := 0; i < num; i++ {
+					idx := rand.Intn(indexNums)
+					fmt.Printf("%s", nums[idx])
+				}
+				break
+			case "*":
+				for i := 0; i < num; i++ {
+					fmt.Printf("%d", (rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)*rangeSpecial)
+				}
+				break
+			case "^":
+				for i := 0; i < num; i++ {
+					outData := rand.Intn(rangeEnd-rangeBegin+1) + rangeBegin
+					//fmt.Printf("begin=%d, end=%d, out=%d\n", rangeBegin, rangeEnd, outData)
+					fmt.Printf("%d", outData*outData)
+				}
+				break
+			}
+			break
+		case "c": //加逗點
+			switch rangeFormat {
+			case "":
+				for i := 0; i < num; i++ {
+					if i != 0 {
+						fmt.Print(",")
+					}
+					fmt.Printf("%d", rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)
+				}
+				break
+			case "&":
+				//fmt.Println("間隔晚點想")
+				indexNums = 0
+				for i := rangeBegin; i <= rangeEnd; i += rangeSpecial {
+					nums[indexNums] = strconv.Itoa(i)
+					indexNums++
+				}
+				for i := 0; i < num; i++ {
+					idx := rand.Intn(indexNums)
+					if i != 0 {
+						fmt.Print(",")
+					}
+					fmt.Printf("%s", nums[idx])
+				}
+				break
+			case "*":
+				for i := 0; i < num; i++ {
+					if i != 0 {
+						fmt.Print(",")
+					}
+					fmt.Printf("%d", (rand.Intn(rangeEnd-rangeBegin+1)+rangeBegin)*rangeSpecial)
+				}
+				break
+			case "^":
+				for i := 0; i < num; i++ {
+					if i != 0 {
+						fmt.Print(",")
+					}
+					outData := rand.Intn(rangeEnd-rangeBegin+1) + rangeBegin
+					//fmt.Printf("begin=%d, end=%d, out=%d\n", rangeBegin, rangeEnd, outData)
+					fmt.Printf("%d", outData*outData)
+				}
+				break
+			}
+			break
+		}
+	}
 	fmt.Println()
 }
 
 func main() {
+	rand.Seed(time.Now().Unix())
 	cnt := 0
-	fmt.Println("Hello, world.")
 	file, err := os.Open("inputdata.txt")
 	if err != nil {
 		panic(err.Error())
@@ -152,12 +368,21 @@ func main() {
 	scanner := bufio.NewScanner(reader)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
-		cnt++
-		fmt.Printf("# %d\n", cnt)
+
 		theLine := bufio.NewScanner(strings.NewReader(scanner.Text()))
+		cnt++
+		fmt.Printf("第 %d筆：", cnt)
+		fmt.Println(scanner.Text())
 		w := readThisLine(theLine)
 		if w[0] != "@as" {
+
 			showData(w)
+
+		} else {
+			//人工測資
+			for i := 0; i < indexNums; i++ {
+				fmt.Print(nums[i] + " ")
+			}
 		}
 		fmt.Println()
 	}
